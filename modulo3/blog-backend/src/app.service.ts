065157dda7a6b06a6f71bffef408d1216497b511
Francisco Higuera <prof.francisco.higuera@ute.edu.ec>
2026-05-18 03:57:33 -0500

    clase 18-05-2025

--- modulo3/blog-backend/src/app.service.ts
+++ modulo3/blog-backend/src/app.service.ts
@@ -49,4 +49,35 @@ export class AppService {
     return this.products!
         .find(product=>product.id===Number(id))!;
   }
+  
+  update(id: string, updatedProductDto: ProductDto): any {
+    const product: ProductDto = this.products!
+        .find(product=>product.id===Number(id))!;
+    if (!product){
+      return;
+    }
+    Object.assign(product,updatedProductDto)
+    return product
+  }
+
+  deleteById(id: string): any {
+    const index = this.products!
+        .findIndex(product=>product.id===Number(id))!;
+    if (index ===-1){
+      return;
+    }
+    const deletedProduct=this.products[index]
+    this.products.splice(index,1);
+    return deletedProduct
+  }
+
+  areaTriangulo(data: any): any {
+    const area = (data.base * data.altura)/2;
+    return {
+      "base": data.base,
+      "altura": data.altura,
+      "areaTriangulo": area,
+    };
+  }
+
 }
