cd874da5f454ec4829c69ce3d741e39d61448c05
Francisco Higuera <prof.francisco.higuera@ute.edu.ec>
2026-06-01 03:57:44 -0500

    clase 2026-06-01

--- modulo3/blog-backend/src/mail/mail.controller.ts
+++ modulo3/blog-backend/src/mail/mail.controller.ts
@@ -5,11 +5,17 @@ import { SuccessResponseDto } from '../common/dtos/response.dto';
 
 @Controller('mail')
 export class MailController {
-  constructor(private readonly mailService: MailService) {}
+  constructor(private readonly mailService: MailService) { }
 
   @Post('gmail')
   async sendGmail(@Body() dto: SendMailDto) {
     const result = await this.mailService.sendMail(dto);
     return new SuccessResponseDto('Correo enviado con Gmail', result);
+  }
+
+  @Get('public-api')
+  async getUsersFromPublicApi() {
+    const result = await this.mailService.fetchUserListFromPublicApi();
+    return new SuccessResponseDto('Usuarios obtenidos', result);
   }
 }
